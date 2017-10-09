f93f7caaf9429406da4d256b940fd95af49b0b8a
root <root@localhost.localdomain>
2017-10-09 03:36:19 -0400

    clean repo

--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("ca fonctionne enfin..\n");
-	return (0);
-}
--- test2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	printf("yolo\n");
-	printf("yolo\n");
-	return (0);
-}
