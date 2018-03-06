570a65fbde1b7f4a0b9c62aabe45c3bd3fe3e4f8
Arthur BONNECARRERE <abonneca@e2r7p17.42.fr>
2018-03-06 10:52:34 +0100

    fixing some norm

--- srcs/helpers_elf.c
+++ srcs/helpers_elf.c
@@ -2,10 +2,10 @@
 
 Elf64_Phdr	*find_last_segment(void *ptr)
 {
-	Elf64_Ehdr *hdr;
-	Elf64_Phdr *phdr;
-	Elf64_Phdr *last_Phdr;
-	int i;
+	Elf64_Ehdr	*hdr;
+	Elf64_Phdr	*phdr;
+	Elf64_Phdr	*last_Phdr;
+	int 		i;
 
 	hdr = ptr;
 	last_Phdr = NULL;
@@ -19,30 +19,29 @@ Elf64_Phdr	*find_last_segment(void *ptr)
 		}
 		phdr = (void *)phdr + sizeof(Elf64_Phdr);
 	}
-	return(last_Phdr);
+	return (last_Phdr);
 }
 
-
-Elf64_Shdr	*get_section64(Elf64_Ehdr *hdr, uint16_t index)
+Elf64_Shdr		*get_section64(Elf64_Ehdr *hdr, uint16_t index)
 {
-	Elf64_Shdr *shdr;
-	int i;
+	Elf64_Shdr	*shdr;
+	int			i;
 
 	shdr = (void *)hdr + hdr->e_shoff;
-	for(i = 0; i < hdr->e_shnum; ++i)
+	for (i = 0; i < hdr->e_shnum; ++i)
 	{
 		if (i == index)
-			return(shdr);
+			return (shdr);
 		shdr = (void *)shdr + sizeof(Elf64_Shdr);
 	}
 	return (NULL);
 }
 
 
-Elf64_Shdr	*get_section64_by_type(Elf64_Ehdr *hdr, uint32_t type)
+Elf64_Shdr		*get_section64_by_type(Elf64_Ehdr *hdr, uint32_t type)
 {
-	Elf64_Shdr *shdr;
-	int i;
+	Elf64_Shdr	*shdr;
+	int			i;
 
 	shdr = (void *)hdr + hdr->e_shoff;
 	for(i = 0; i < hdr->e_shnum; ++i)
@@ -55,34 +54,33 @@ Elf64_Shdr	*get_section64_by_type(Elf64_Ehdr *hdr, uint32_t type)
 }
 
 
-Elf64_Shdr	*get_section64_with_e(Elf64_Ehdr *hdr, Elf64_Addr entry)
+Elf64_Shdr		*get_section64_with_e(Elf64_Ehdr *hdr, Elf64_Addr entry)
 {
-	Elf64_Shdr *shdr;
-	int i;
+	Elf64_Shdr	*shdr;
+	int			i;
 
 	shdr = (void *)hdr + hdr->e_shoff;
 	for(i = 0; i < hdr->e_shnum; ++i)
 	{
 		if (entry >= shdr->sh_addr && entry < shdr->sh_addr + shdr->sh_size)
-			return(shdr);
+			return (shdr);
 		shdr = (void *)shdr + sizeof(Elf64_Shdr);
 	}
 	return (NULL);
 }
 
 
-Elf64_Shdr	*get_sym_strtab(Elf64_Ehdr *hdr)
+Elf64_Shdr		*get_sym_strtab(Elf64_Ehdr *hdr)
 {
-	Elf64_Shdr *shdr;
-	int i;
+	Elf64_Shdr	*shdr;
+	int			i;
 
 	shdr = (void *)hdr + hdr->e_shoff;
 	for(i = 0; i < hdr->e_shnum; ++i)
 	{
 		if (i != hdr->e_shstrndx && shdr->sh_type == SHT_STRTAB)
-			return(shdr);
+			return (shdr);
 		shdr = (void *)shdr + sizeof(Elf64_Shdr);
 	}
 	return (NULL);
 }
-
